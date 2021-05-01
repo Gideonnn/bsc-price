cc08ce569c98877f5c0c4de96dfe005b1979071e
Gideon Spierings <gideongideon@gmail.com>
2021-05-01 11:25:42 +0200

    Add links to pancakeswap and poocoin

--- src/components/TokenTable.js
+++ src/components/TokenTable.js
@@ -7,6 +7,9 @@ function TokenTableHead() {
         <th>Name</th>
         <th>Price ($)</th>
         <th>Price (€)</th>
+        <th>Poocoin</th>
+        <th>Pancakeswap</th>
+        <th>Address</th>
       </tr>
     </thead>
   );
@@ -23,6 +26,35 @@ function TokenTableRow({ token, onToggleFavorite }) {
         <td>{token.name}</td>
         <td>{(token.price * 0.83).toFixed(8)}</td>
         <td>{(token.price * 0.83).toFixed(8)}</td>
+        <td>
+          <a
+            href={`https://poocoin.app/tokens/${token.address}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Chart
+          </a>
+        </td>
+        <td className="justify-content">
+          <a
+            href={`https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=${token.address}&inputCurrency=BNB`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            V1
+          </a>
+          &nbsp;
+          <a
+            href={`https://exchange.pancakeswap.finance/#/swap?outputCurrency=${token.address}&inputCurrency=BNB`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            V2
+          </a>
+        </td>
+        <td>
+          <button>Copy</button>
+        </td>
       </tr>
     </tbody>
   );
